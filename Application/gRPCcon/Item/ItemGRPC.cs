5255cc899c690d38d4c5e9e856d9e7548d785537
ricardofernandes21 <114725680+ricardofernandes21@users.noreply.github.com>
2023-11-01 09:58:24 +0100

    gitignore

--- Application/gRPCcon/Item/ItemGRPC.cs
+++ Application/gRPCcon/Item/ItemGRPC.cs
@@ -15,13 +15,13 @@ public class ItemGRPC : IItemGRPC
 
        public Task<Domain.Models.Item> CreateAsync(Domain.Models.Item item)
        {
-              GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:9090");
+              GrpcChannel channel = GrpcChannel.ForAddress("http://localhost:9090");
               var client = new ItemService.ItemServiceClient(channel);
 
               var itemToSend = new ItemP
               {
                      Name = item.Name,
-                     Price = (double)item.Price
+                     Price = item.Price
               };
               
               client.postItem(itemToSend);
--- Domain/Models/Item.cs
+++ Domain/Models/Item.cs
@@ -6,10 +6,10 @@ public class Item
     public string Name { get; set; }
     public string? Description { get; set;}
     public string? Category { get; set; }
-    public double? Price { get; set; }
+    public double Price { get; set; }
     public int? Stock { get; set; }
 
-    public Item(string name, double? price)
+    public Item(string name, double price)
     {
         Name = name;
         Price = price;
