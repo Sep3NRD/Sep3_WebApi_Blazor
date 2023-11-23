a80c84fc1ab90301285f8d322ae42b710d12cbd3
duart23 <331347@via.dk>
2023-11-23 06:19:22 +0100

    Update CreateItemTesting.cs

--- Test/CreateItemTesting.cs
+++ Test/CreateItemTesting.cs
@@ -3,30 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using WebApp.Controllers;
 
 namespace TestProject1;
+
 [TestFixture]
 public class CreateItemTesting
 {
-    public void AddProduct_ValidProduct_RedirectToAdminDashboard()
-    {
-         var productServiceMock = new Mock<ProductService.ProductServiceClient>();
-        productServiceMock.Setup(x => x.AddProduct(It.IsAny<AddProductRequest>(), It.IsAny<CallOptions>()))
-                          .Returns(new AddProductResponse { Success = true });
-
-        var adminController = new AdminController(productServiceMock.Object);
-
-        var newProduct = new Product
-        {
-            ProductName = "Test Product",
-            Description = "This is a test product.",
-            Price = 19.99m,
-            QuantityInStock = 100
-        };
-
-        // Act
-        var result = adminController.AddProduct(newProduct) as RedirectToActionResult;
-
-        // Assert
-        Assert.IsNotNull(result);
-        Assert.AreEqual("AdminDashboard", result.ActionName);
     
 }
